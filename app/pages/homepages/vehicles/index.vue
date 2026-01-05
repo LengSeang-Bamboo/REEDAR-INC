0e0df99206e6d15fd893e5c4e720742ed44f08d7
LengSeang-Bamboo <khemseangleng@gmail.com>
2026-01-05 17:02:49 +0700

    Fix image import path

--- app/pages/homepages/vehicles/index.vue
+++ app/pages/homepages/vehicles/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useDashboard } from "@nuxt/ui/runtime/utils/dashboard.js";
-import bgimg from "../assets/images/rolls-royce-phantom-5120x2880-23258.jpg"
+import bgimg from "../../assets/images/rolls-royce-phantom-5120x2880-23258.jpg"
 import { cars } from "~/data/cars";
 
 </script>
